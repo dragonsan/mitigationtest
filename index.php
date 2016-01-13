dc882b4049d9f83068f8afb12b420996523a07a7
dragonsan <jbtb07@yahoo.com>
2016-01-13 15:56:19 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="utf-8"><title>ohaidere.</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <style>html,body{font-family:'Roboto',sans-serif;text-align:center}</style>
+  </head>
+  <body>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YUAmjntSn4Y?autoplay=1&modestbranding=1" frameborder="0" allowfullscreen style="margin:0 auto;margin-top:50px;margin-bottom:5px"></iframe>
+    <h1>oHai.</h1>Dragon - HF<br />
+    <small><?php echo rand(0,9999999); ?></small>
+  </body>
+</html>
